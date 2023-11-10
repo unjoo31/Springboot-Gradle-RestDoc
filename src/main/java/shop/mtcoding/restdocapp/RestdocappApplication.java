2d956865292752e067e6d301d49a7d47003badcd
unjoo31 <unjoo31@gmail.com>
2023-11-10 10:15:55 +0900

    프로젝트생성

---
+++ src/main/java/shop/mtcoding/restdocapp/RestdocappApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.restdocapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestdocappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestdocappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/restdocapp/RestdocappApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.restdocapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestdocappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
