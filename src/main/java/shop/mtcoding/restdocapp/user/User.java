31550f7cb9a23374206bd372c6dd631cbbd68ce1
unjoo31 <unjoo31@gmail.com>
2023-11-10 10:54:59 +0900

    세팅완료

---
+++ src/main/java/shop/mtcoding/restdocapp/user/User.java
@@ -0,0 +1,43 @@
+package shop.mtcoding.restdocapp.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_tb")
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 무조건 있어야함 : 오브젝트 매핑때 기본적으로 default constractor를 때리는데 없으면 안됨
+@Getter
+public class User {
+    @Id // import javax.persistence.Id; -> jacarta?
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, length = 20)
+    private String username;
+    @Column(unique = true, length = 100)
+    private String password;
+    @Column(unique = true, length = 100)
+    private String email;
+
+    // 메서드를 만들때는 의미있는 이름으로 만들기
+    public void updatePassword(String password){
+        this.password = password;
+    }
+
+    @Builder
+    public User(Integer id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+}
---
+++ src/main/java/shop/mtcoding/restdocapp/user/UserController.java
@@ -0,0 +1,11 @@
+package shop.mtcoding.restdocapp.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    
+}
